e0aa1841ad555b462b0a7203272e23d52c962859
250909895 <250909895@qq.com>
2025-09-26 15:46:17 +0800

    SMWT定制

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://papen.com.cn".to_owned()
+    "http://papen.com.cn:21114".to_owned()
 }
 
 #[inline]
